e3366ae42ec40cde7664081105673a0301b29647
Nicolas <nicolasmajorczyk@gmail.com>
2023-03-01 17:09:10 +0100

    travail sur patients

--- src/main/java/com/an2m/repository/Suivi_patientRepo.java
+++ src/main/java/com/an2m/repository/Suivi_patientRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.an2m.model.Suivi_patient;
 
 public interface Suivi_patientRepo extends JpaRepository<Suivi_patient, Integer> {
-
+		
 }
--- src/main/java/com/an2m/service/An2mService.java
+++ src/main/java/com/an2m/service/An2mService.java
@@ -1,15 +1,23 @@
 package com.an2m.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.an2m.dto.SuiviPatientDto;
+import com.an2m.model.Patient;
+import com.an2m.model.Suivi_patient;
+import com.an2m.model.User;
 import com.an2m.repository.ChambreRepository;
 import com.an2m.repository.LitsRepo;
 import com.an2m.repository.PatientRepo;
 import com.an2m.repository.Suivi_patientRepo;
+import com.an2m.repository.UserRepository;
 @Component
 public class An2mService {
-
+	@Autowired
+	UserRepository userRepo;
 	@Autowired
 	private ChambreRepository chambresRepo;
 	@Autowired
@@ -44,5 +52,49 @@ public class An2mService {
 		this.suivi_patientRepo = suivi_patientRepo;
 	}
 	
-
+	  public void createSuiviPatient (SuiviPatientDto suiviPatientDto) {
+	    	
+	    	 System.out.println(suiviPatientDto);
+	        Suivi_patient suivi = new Suivi_patient();
+	        
+	        
+	        suivi.setId((int) suiviPatientDto.getId());
+	        suivi.setCom(suiviPatientDto.getCom());
+	        
+	        
+	        User user ;
+	        user=userRepo.findById(suiviPatientDto.getUserId()).get();
+	        suivi.setUser(user);
+	        System.out.println(user.getId());
+	        Optional<Patient> patient ;
+	        patient= patientRepo.findById(suiviPatientDto.getPatientId());
+	        suivi.setPatient(patient.get());
+	        
+	        
+	        
+	       /* suivi.setCom(suiviPatientDto.getCom());
+	        System.out.println(suiviPatientDto.getCom());
+	        
+	        
+	       Optional<User> user ;
+	        //user.setId(suiviPatientDto.getUserId());
+	       System.out.println("iduser="+suiviPatientDto.getUserId());
+	        user=userRepo.findById(suiviPatientDto.getUserId());
+	        //Patient patient = new Patient();
+	        //patient.setId(suiviPatientDto.getPatientId());
+	        
+	        System.out.println(suiviPatientDto.getPatientId());
+	        Optional<Patient> patient ;
+	        patient= patientRepo.findById(suiviPatientDto.getPatientId());
+	        
+	        
+	        suivi.setUser(user.get());
+	        
+	        System.out.println(user.get());
+	       
+	       suivi.setPatient(patient.get());
+	        */ 
+	        suivi_patientRepo.save(suivi);    
+	        System.out.println("yo"+suivi.getCom());
+	    }
 }
